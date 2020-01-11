49130c17cc8a2d90eb3cf6e824ae0ae6c9510eac
Fabio Vieira <fabio.luizvieira@globo.com>
2020-01-11 12:14:03 -0300

    Criando a classe Posicao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace xadrex_console
 {
@@ -10,6 +7,11 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
         }
     }
 }
